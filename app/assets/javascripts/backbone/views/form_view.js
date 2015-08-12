c5598ec93a071ee0079833643020b27103ffd91d
Owens O'Brien <owensobrien@gmail.com>
2015-08-11 21:26:38 -0400

    creating form submission functionality

--- app/assets/javascripts/backbone/views/form_view.js
+++ app/assets/javascripts/backbone/views/form_view.js
@@ -1,5 +1,8 @@
 App.Views.Forms = Backbone.View.extend({
 	el:'#page',
+	events: {
+    'submit form#help-needed': 'addVictim'
+  },
 	initialize: function(){
 		console.log('Form View Loaded');
 		this.template = HandlebarsTemplates['help_form'];
@@ -8,4 +11,14 @@ App.Views.Forms = Backbone.View.extend({
 	render: function(){
 		this.$el.html(this.template);
 	},
+	addVictim: function(){
+		alert('this is working');
+		var data = { 
+			name: $('[name=name]').val(),
+			age: $('[name=age]').val(),
+			gender: $('[name=gender]').val(),
+			location: $('[name=location]').val()
+		 };
+		// this.collection.create(data);
+	}
 });
