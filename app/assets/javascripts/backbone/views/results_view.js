8fcf6576a1a6fb6f0f76fd70777ea2a5bcc1e282
Owens O'Brien <owensobrien@gmail.com>
2015-08-14 11:42:38 -0400

    cleaning and commenting

--- app/assets/javascripts/backbone/views/results_view.js
+++ app/assets/javascripts/backbone/views/results_view.js
@@ -1,12 +1,9 @@
 App.Views.Results = Backbone.View.extend({
 	initialize: function(){
-		console.log('listing results');
 		this.template = HandlebarsTemplates['search_result'];
-		// this.listenTo(this.collection, 'add', render);
 		this.render();
 	},
 	render: function(){
-		console.log('rendering');
 		var modelData = this.model.toJSON();
 		var compiledTemplate = this.template(modelData);
 		$('.victim-list').append(compiledTemplate);
--- app/assets/javascripts/backbone/views/search_view.js
+++ app/assets/javascripts/backbone/views/search_view.js
@@ -1,49 +1,47 @@
 App.Views.Search = Backbone.View.extend({
-	//sets id of div where main rendering will take place
+    //sets id of div where main rendering will take place
     el: '#page',
     initialize: function() {
-    	//sets template
+        //sets template
         this.template = HandlebarsTemplates['search_box'];
-        this.listenTo(this.collection, 'add', console.log('new vic added')); /////Not Working
         this.render();
         this.renderAllItems();
     },
     renderAllItems: function() {
-    	//Empties div, inserts table template and then sends each model in the collection to the render function, from newest to oldest
-    	$('#victim-box').empty();
+        //Empties div, inserts table template and then sends each model in the collection to the render function, from newest to oldest
+        $('#victim-box').empty();
         $('#victim-box').html(HandlebarsTemplates['search_table']);
         this.collection.reversed().each(this.renderItem, this);
     },
     renderItem: function(model) {
-    	//Sends each model into the View to be rendered
+        //Sends each model into the View to be rendered
         var newVictimView = new App.Views.Victim({
             model: model
         });
     },
     render: function() {
-    	//renders search page
+        //renders search page
         this.$el.html(this.template);
     },
     events: {
-    	//click event for search button
+        //click event for search button
         // 'click .search': 'goSearch',
         'keyup': 'goSearch',
-        'click [type="radio"]':'goSearch',
-      //click event for sign up button
+        'click [type="radio"]': 'goSearch',
+        //click event for sign up button
         'click .sign-up': 'loadSignup',
-      //click event for log in button  
+        //click event for log in button  
         'click .log-in': 'loadLogin'
 
     },
     goSearch: function() {
-    	//extracts values from search fields
+        //extracts values from search fields
         var data = {
             name: $('[name=name]').val(),
             location: $('[name=location]').val(),
             date: $('#datepicker').val(),
             need_Rescue: $('input[name="safe"]:checked').val()
         };
-        console.log(data);
         //Empties div and mounts table for results
         $('#victim-box').empty();
         $('#victim-box').html(HandlebarsTemplates['search_table']);
@@ -57,48 +55,47 @@ App.Views.Search = Backbone.View.extend({
 
 
         for (var l = 0; l < this.collection.length; l++) {
-        	console.log(this.collection.models[l].attributes.need_rescue);
-        	var searchSafe = this.collection.models[l].attributes.need_rescue;
+            var searchSafe = this.collection.models[l].attributes.need_rescue;
             if (searchSafe.toString() == data.need_Rescue) {
                 safeFilter.add(this.collection.models[l]);
             }
-        	}
-        	console.log(safeFilter);
+        }
         for (var i = 0; i < safeFilter.length; i++) {
-        	var searchName = safeFilter.models[i].attributes.name.toLowerCase();
+            var searchName = safeFilter.models[i].attributes.name.toLowerCase();
             if (searchName.match(data.name.toLowerCase())) {
                 nameFilter.add(safeFilter.models[i]);
             }
-        	}
-        	console.log(nameFilter);
-        	for (var k = 0; k < nameFilter.length; k++) {
-        		var searchLoc = nameFilter.models[k].attributes.location.toLowerCase();
+        }
+        for (var k = 0; k < nameFilter.length; k++) {
+            var searchLoc = nameFilter.models[k].attributes.location.toLowerCase();
             if (searchLoc.match(data.location.toLowerCase())) {
                 locationFilter.add(nameFilter.models[k]);
             }
-        	}
-        	console.log(locationFilter);
-        for (var j = 0; j<locationFilter.length; j++){
-        	var searchDate = locationFilter.models[j].attributes.convCreatedDate;
-          if ((searchDate == data.date) || (data.date === "")){
-             dateFilter.add(locationFilter.models[j]);
-	          var newResultView = new App.Views.Results({
-	              model: dateFilter.models[j]
-	          });
         }
-        console.log(dateFilter);
-      }
+        for (var j = 0; j < locationFilter.length; j++) {
+            var searchDate = locationFilter.models[j].attributes.convCreatedDate;
+            if ((searchDate == data.date) || (data.date === "")) {
+                dateFilter.add(locationFilter.models[j]);
+                var newResultView = new App.Views.Results({
+                    model: dateFilter.models[j]
+                });
+            }
+        }
 
-        
-	},
-	loadSignup: function(){
-        $('#search-bar').empty();
-        App.seekers = new App.Collections.Seekers();
-        App.signup = new App.Views.Signups({collection: App.seekers});
+
     },
-    loadLogin: function(){
+    loadSignup: function() {
         $('#search-bar').empty();
         App.seekers = new App.Collections.Seekers();
-        App.login = new App.Views.Logins({collection: App.seekers});
+        App.signup = new App.Views.Signups({
+            collection: App.seekers
+        });
+    },
+    loadLogin: function() {
+        $('#search-bar').empty();
+        App.seekers = new App.Collections.Seekers();
+        App.login = new App.Views.Logins({
+            collection: App.seekers
+        });
     }
 });
