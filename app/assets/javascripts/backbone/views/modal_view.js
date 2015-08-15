a4defad35fbeff198cfb31ef9ce89fb5efb26ce8
Rubina Pradhan <pradhanr03@gmail.com>
2015-08-15 19:20:52 -0400

    comments working

--- app/assets/javascripts/backbone/views/modal_view.js
+++ app/assets/javascripts/backbone/views/modal_view.js
@@ -21,12 +21,34 @@ App.Views.Modal = Backbone.View.extend({
 	returnToSearch: function() {
 		this.$el.empty();
 		this.$el.fadeOut(100);
+		// this.unbind();
+		// this.destroy();
+		this.undelegateEvents();
+		this.stopListening();
 	},
 	showComments: function() {
 		console.log('show my comments bro!');
-		    var comment = $('[name=comment]').val();
+		var id = $('#victim-id-comment').val();
+		
+		 $.ajax({
+              type: 'GET',
+              url: '/sessions/new',
+              success: function(data) {
+
+              	console.log(data);
+            var comment = $('[name=comment]').val();
+		    var data1 = {
+		    		description: $('[name=comment]').val(),
+		    		victim_id: id,
+		    		seeker_id: data		    		
+		    }
             var tag = $('<p>').html(comment).addClass('showComment');
+
         $('#comment-box').append(tag);
+        App.comments = new App.Collections.Comments();
+        App.comments.create(data1);
+              }
+        });    
 
 	}
 });
