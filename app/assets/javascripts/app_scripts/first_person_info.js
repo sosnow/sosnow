dd1856ede5666c649e25007ee3da47ad72d29bf2
Jorge Bustos-Estefan <jorbues@yahoo.com>
2015-08-16 14:43:14 -0400

    prioritized geolocation AJAX responses

--- app/assets/javascripts/app_scripts/first_person_info.js
+++ app/assets/javascripts/app_scripts/first_person_info.js
@@ -1,7 +1,11 @@
   // Values for the first person (initial form submission):
 
   var firstPersonName = undefined;
+  var firstPersonLocationFromIp = undefined;
+  var firstPersonLocationFromHtml5 = undefined;
   var firstPersonLocation = undefined;
+  var firstPersonGeolocationFromIp = undefined;
+  var firstPersonGeolocationFromHtml5 = undefined;
   var firstPersonGeolocation = undefined;
   var firstPersonDescription = undefined;
   var firstPersonPhone = undefined;
